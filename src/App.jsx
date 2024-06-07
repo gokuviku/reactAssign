3fcca87925c901740cdb80517dd7b30b9a4b0f4a
Vishal-8155 <69633165+gokuviku@users.noreply.github.com>
2024-06-07 10:44:53 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import CalcClass from "./components/CalcClass"
 import CalsFun from "./components/CalsFun"
 import Counter from "./components/Counter"
 import ES6 from "./components/ES6"
+import OnChangeTextClass from "./components/OnChangeTextClass"
+import OnChangeTextFun from "./components/OnChangeTextFun"
 import TenQuestion from "./components/TenQuestion"
 
 
@@ -12,8 +14,10 @@ function App() {
     <>
       <CalsFun />
       <CalcClass />
-      <ES6/>
       <Counter/>
+      <OnChangeTextFun />
+      <OnChangeTextClass/>
+      <ES6/>
       <TenQuestion />
     </>
   )
